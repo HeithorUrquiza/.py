4dafd8df9e43feda5dc055baf63f940060275cba
Heithor Urquzia <111160912+HeithorUrquiza@users.noreply.github.com>
2022-12-19 18:54:44 -0300

    Add comentarios

---
+++ Códigos/Comentarios.py
@@ -0,0 +1,20 @@
+"""
+    - import this -> Zen do Python
+    - Camel Case para nome de classes
+    - letras minúsculas e underline para nomes de variáveis e funções
+    
+    ___________________________________________________________________________________________________________
+    
+    DIR -> Apresenta todos os atributos e funções/métodos disponíveis para determinado tipo de dado ou variável
+    
+    - dir(tipo de dados/variável)
+    
+    HELP -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos deisponíveis para determinado tipo de dado ou variável
+    
+    - help(tipo de dado/variável.propriedade)
+"""
+
+print(dir("olá"))
+
+
+

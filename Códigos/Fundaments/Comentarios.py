9c32ede4bd835e0af8596cfaae9d8b50265be204
Heithor Urquzia <111160912+HeithorUrquiza@users.noreply.github.com>
2022-12-20 16:53:21 -0300

    Att docs

---
+++ Códigos/Fundaments/Comentarios.py
@@ -0,0 +1,30 @@
+"""
+    - import this -> Zen do Python
+    - Camel Case para nome de classes
+    - letras minúsculas e underline para nomes de variáveis e funções
+    
+    _________________________________________________________________________________________________________________________________
+    
+    DIR -> Apresenta todos os atributos e funções/métodos disponíveis para determinado tipo de dado ou variável
+    
+    - dir(tipo de dados/variável)
+    
+    HELP -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos deisponíveis para determinado tipo de dado ou variável
+    
+    - help(tipo de dado/variável.propriedade)
+    
+    _________________________________________________________________________________________________________________________________
+    
+    RECEBENDO DADOS DO USUÁRIO
+    
+    nome = input(str("Qual o seu nome? "))  #Entrada de dados
+    print(f"Seja Bem-vindo(a) {nome}") #Saída de dados
+    
+    - Exemplos de PRINT
+    print('Seja bem-vindo %s' % nome)    
+    print('Seja bem-vindo {0}'.fromat(nome))    
+    print(f'Seja bem-vindo {nome}')
+    
+    * CAST -> conversão de um tipo de dado para outro    
+    
+"""

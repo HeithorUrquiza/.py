d61959c7a9434867fa3ae73eb063d182ff770366
Heithor Urquzia <heithoru@gmail.com>
2023-01-23 15:41:16 -0300

    Att Anotacoes

--- forfun/BB/teste.py
+++ forfun/BB/teste.py
@@ -1,8 +1,1 @@
-import pandas as pd
-import numpy as np
-
-concursos = pd.read_csv('C:\\Users\\heith\\OneDrive\\Documentos\\GitHub\\Python_cods\\forfun\\BB\\concursos.csv', ';')
-
-print(concursos.info()) #Informações sobre o DataFrame
-print(concursos.head()) #Mostra os 5 primeiros elem por padrão tail() mostra os últimos 5
 

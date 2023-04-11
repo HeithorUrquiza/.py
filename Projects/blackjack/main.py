17795d0417ec1b69173777621ee775c74ffd201c
Heithor Urquzia <heithoru@gmail.com>
2023-04-11 14:53:27 -0300

    Started blackjack project

---
+++ Projects/blackjack/main.py
@@ -0,0 +1,77 @@
+import random
+from art import logo
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
+
+
+def hit(list_card):
+    while True:
+        answer = input("Do you wanna get another card? ").lower()
+            
+        if answer == "yes":
+            list_card.append(deal_card())
+        else:
+            return list_card
+     
+        
+def calculate_score(user, computer):
+    users_score = sum(user)
+    computers_score = sum(computer)
+    
+    if users_score == 21:
+        return 0
+    elif users_score > 21:
+        return 1
+    elif users_score > 21 and user.__contains__(11):
+        user[user.index(11)] = 1
+        users_score = sum(user)
+        
+        if users_score > 21:
+            return 1
+        elif users_score < 21:
+            user = hit()
+            users_score = sum(user)
+    elif users_score < 21:
+            user = hit()
+            users_score = sum(user)
+            
+    if computers_score == 21:
+        return 1
+    elif computers_score > 21:
+        return 0
+    elif computers_score > 21 and computers_score.__contains__(11):
+        computer[computer.index(11)] = 1
+        computers_score = sum(computer)
+        
+        if computers_score > 21:
+            return 0
+        elif computers_score < 21:
+            computer = hit()
+            computers_score = sum(computer)    
+    elif computers_score < 17:
+        computer = hit()
+        computers_score = sum(computer)
+        
+    if users_score > computers_score:
+        return 0
+    elif users_score < computers_score:
+        return 1
+    else:
+        return 2    
+
+
+def play():
+    users_cards = []
+    computers_cards = []
+
+    users_cards.append(deal_card())
+    users_cards.append(deal_card())
+    computers_cards.append(deal_card())
+    computers_cards.append(deal_card())
+
+
+    
+    
+    
--- Projects/calculator/main.py
+++ Projects/calculator/main.py
@@ -1,4 +1,5 @@
 from art import logo
+from os import system
 from operations import *
 
 
@@ -6,28 +7,26 @@ def calculator():
     
     print(logo)
     num1 = float((input("Type the first number: ")))
-    operation = input("What's the operation: [+, -, *, /]: ")
-    num2 = float((input("Type the second number: ")))
-
-    function = operations[operation] 
-    answer = function(num1, num2)
-    
-    print(f"\n{num1} {operation} {num2} = {answer}")
+    should_continue = False
     
     while True:
-        will_continue = input(f"\nType 'y' to continue calculating with {answer}, or type 'n' to exit: ").lower()
+        operation = input("What's the operation: [+, -, *, /]: ")
+        num2 = float((input("Type the next number: ")))
+        function = operations[operation] 
+        answer = function(num1, num2)
         
-        if will_continue == "y":
-            new_operation = input("\nPick an operation: ")
-            num3 = float(input("What's the next number: "))
-            function = operations[new_operation]
-            new_answer = function(answer, num3)
-        else: 
+        print(f"\n{num1} {operation} {num2} = {answer}")
+        
+        action = input(f"\nType 'y' to continue calculating with {answer}, 'new' to a new calculate or 'n' to exit: ")
+        
+        if action == 'y':
+            num1 = answer
+        elif action == 'new':
+            system('cls')
+            calculator()
+        else:
             return
             
-        print(f"\n{answer} {new_operation} {num3} = {new_answer}")
-        answer = new_answer
-        
 calculator()
             
     

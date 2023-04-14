4c4a59d351ebf71a2a597b2c0d5179e4e3d3a701
Heithor Urquzia <heithoru@gmail.com>
2023-04-14 15:02:01 -0300

    Finished higher_lower game and Starting coffee_machine

--- Projects/higher_lower/main.py
+++ Projects/higher_lower/main.py
@@ -4,10 +4,12 @@ from os import system
 import random
 
 def random_person():
+    '''Generate a random person'''
     return random.choice(data)
 
 
 def compare_person(followers1, followers2):
+    '''Compare followers number of two people'''
     if followers1 > followers2:
         return 'A'
     else: 
@@ -15,34 +17,46 @@ def compare_person(followers1, followers2):
     
 
 def higher_lower_message(A, B):
-    print(logo)
-    print(f"\nCompare A: {A['name']}, {A['description']}, from {A['country']}")
+    '''Format a printable message'''
+    print(f"Compare A: {A['name']}, {A['description']}, from {A['country']}")
     print(vs)
     print(f"Against B: {B['name']}, {B['description']}, from {B['country']}")
-        
     
-
-personA = random_person()
-personB = random_person()
-user_score = 0
-
-should_continue = True
-
-higher_lower_message(personA, personB)
-
-while should_continue:
-    more_follwers = compare_person(personA['follower_count'], personB['follower_count'])
-    user_input = input("Which one you guess has more followers? Type 'A' or 'B': ").upper()
-
-    if user_input == more_follwers:
-        system('cls')
-        user_score += 1
+    
+def check_repeat(A, B):
+    '''Check if the raffled people are the same'''
+    while A == B:
+        B = random_person()
+    return B
+        
+        
+def play_game():
+    '''Run the game'''
+    personB = random_person()
+    should_continue = True
+    user_score = 0
+    
+    print(logo)
+    
+    while should_continue:
         personA = personB
         personB = random_person()
-        
-        print(f"Right choice. Current score: {user_score}")
+        personB = check_repeat(personA, personB)
+
         higher_lower_message(personA, personB)
-    else:
+
+        more_follwers = compare_person(personA['follower_count'], personB['follower_count'])
+        user_input = input("Which one you guess has more followers? Type 'A' or 'B': ").upper()
+
         system('cls')
-        print(f"Wrong choice. Your final score is: {user_score}")
-        should_continue = False
+        print(logo)
+        
+        if user_input == more_follwers:
+            user_score += 1
+            print(f"Right choice. Current score: {user_score}")
+        else:
+            print(f"\nWrong choice. Your final score is: {user_score}\n")
+            should_continue = False
+
+
+play_game()

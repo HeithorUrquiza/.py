374abcb2bd84ab8c837e57034fe6f647254e65e8
Heithor Urquzia <heithoru@gmail.com>
2023-05-09 16:43:19 -0300

    Pomodoro project initialized

--- AM/main.py
+++ AM/main.py
@@ -1,7 +1,6 @@
 # import os
 # import tarfile
 # import urllib
-import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np  
 from zlib import crc32
@@ -9,76 +8,17 @@ from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder
 
+#Download the Data
 PATH = "AM/housing.csv"
-
-# DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
-# HOUSING_PATH = os.path.join("datasets", "housing")
-# HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
-
-# def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
-#     os.makedirs(housing_path, exist_ok=True)
-#     tgz_path = os.path.join(housing_path, "housing.tgz")
-#     urllib.request.urlretrieve(housing_url, tgz_path)
-#     housing_tgz = tarfile.open(tgz_path)
-#     housing_tgz.extractall(path=housing_path)
-#     housing_tgz.close()
-    
-    
+      
+import pandas as pd 
+      
 def load_housing_data(housing_path=PATH):
     return pd.read_csv(housing_path)
 
-
-def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
-
-
-def test_set_check(identifier, test_ratio):
-    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2 ** 32
-
-
-def split_train_test_by_id(data, test_ratio, id_column):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_:test_set_check(id_, test_ratio))
-    return data.loc[~in_test_set], data.loc[in_test_set]
-
-
+#Take a Quick Look at the Data Structure
 housing = load_housing_data()
-housing_wiht_id = housing.reset_index()
-housing_wiht_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
-housing["income_cat"] = pd.cut(housing["median_income"], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1, 2, 3, 4, 5])
+print(housing.head())
+print(housing.info())
 
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 
-for train_index, test_index in split.split(housing, housing["income_cat"]):
-    strat_train_set = housing.loc[train_index]
-    strat_test_set = housing.loc[test_index]
-
-#train_set, test_set = split_train_test_by_id(housing_wiht_id, 0.2, "id")
-
-for set_ in (strat_train_set, strat_test_set):
-    set_.drop("income_cat", axis=1, inplace=True)
-    
-housing = strat_train_set.drop("median_house_value", axis=1)
-housing_labels = strat_train_set["median_house_value"].copy()
-
-median = housing["total_bedrooms"].median()
-housing["total_bedrooms"].fillna(median, inplace=True)
-
-imputer = SimpleImputer(strategy="median")
-housing_num = housing.drop("ocean_proximity", axis=1)
-imputer.fit(housing_num)
-
-X = imputer.transform(housing_num)
-housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=housing_num.index)
-
-housing_cat = housing[["ocean_proximity"]]
-
-#Classificando textos
-ordinal_encoder = OrdinalEncoder()
-housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
-
-print(housing_cat_encoded[:10])

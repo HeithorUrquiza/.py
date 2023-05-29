81b61f4454cf561e33e494b9528d29587b213060
Heithor Urquzia <heithoru@gmail.com>
2023-05-28 22:45:25 -0300

    Att account and phones in flight-deals project

--- Playgroung/testeTwilio.py
+++ Playgroung/testeTwilio.py
@@ -3,7 +3,7 @@ import os
 from twilio.rest import Client
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
-account_sid = "AC7f282388cfdf26268f0a6fa117374c78"
+""" account_sid = "AC7f282388cfdf26268f0a6fa117374c78"
 auth_token = "52f2e8227d786cf3fdeb0718f58ef40e"
 client = Client(account_sid, auth_token)
 message = client.messages.create(
@@ -11,12 +11,12 @@ message = client.messages.create(
   from_="+12542726479",
   to="+5562985002235"
 )
-print(message.sid)
+print(message.sid) """
 
-""" from twilio.rest import Client
+from twilio.rest import Client
 
-account_sid = 'AC7f282388cfdf26268f0a6fa117374c78'
-auth_token = 'c27c7766eab0bc03bfa57c147afce0b4'
+account_sid = 'ACd36a1c0e698394be9e6695bef7d51280'
+auth_token = 'a48ccce8fc751e807a1a132fec753ec9'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
@@ -25,4 +25,4 @@ message = client.messages.create(
   to='whatsapp:+556285002235'
 )
 
-print(message.sid) """
+print(message.sid)

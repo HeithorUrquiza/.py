3ee957b88671bf1df5c30f752c5e07f15f77666b
Heithor Urquzia <heithoru@gmail.com>
2023-06-27 10:52:24 -0300

    Practicing with Bootstrap

--- Códigos/IFG/tarefas_inicias/ListaFuncionario.py
+++
@@ -1,9 +0,0 @@
-print("\t\n*** Relátorio de Pagamentos ***")
-relat = str(input("\nInsira o nome do arquivo que deseja abrir e sua exntenção: "))
-print(" ")
-with open(relat, 'r') as arquivo:
-    for value in arquivo:
-        print(value)
-
-'''arquivo = open("salario_funcionario.txt", "r")
-print(arquivo.readlines())'''

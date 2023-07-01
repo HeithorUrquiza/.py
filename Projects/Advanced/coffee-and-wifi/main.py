8287c0bd443882c1ceaa0e13c793c496ab8d8ee4
Heithor Urquzia <heithoru@gmail.com>
2023-06-30 22:04:54 -0300

    Learning about SQLite

--- Projects/Advanced/coffee-and-wifi/main.py
+++ Projects/Advanced/coffee-and-wifi/main.py
@@ -2,16 +2,24 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, URL
 import csv
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+secret_key = os.urandom(24).hex()
+app.config['SECRET_KEY'] = secret_key
 Bootstrap5(app)
 
 
 class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
+    cafe = StringField('Cafe name', validators=[DataRequired(message="Field is required")])
+    location = StringField('Cafe Location on Google Maps (URL)', validators=[DataRequired(message="Field is required"), URL(require_tld=False)])
+    open_time = StringField('Opening Time e.g. 8AM', validators=[DataRequired(message="Field is required")])
+    close_time = StringField('Closing Time e.g. 5:30PM', validators=[DataRequired(message="Field is required")])
+    cf_rating = StringField('Coffee Rating (0-5)', validators=[DataRequired(message="Field is required")])
+    wf_rating = StringField('Wifi Strength Rating (0-5)', validators=[DataRequired(message="Field is required")])
+    pw = StringField('Power Socket Availability (0-5)', validators=[DataRequired(message="Field is required")])
     submit = SubmitField('Submit')
 
 # Exercise:
@@ -29,16 +37,26 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route('/add', methods=["GET", "POST"])
 def add_cafe():
     form = CafeForm()
-    if form.validate_on_submit():
-        print("True")
     # Exercise:
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
-    return render_template('add.html', form=form)
-
+    if form.validate_on_submit():
+        print("True")
+        with open('Projects\Advanced\coffee-and-wifi\cafe-data.csv', mode="a", newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([
+                f"{form.cafe.data}", 
+                f"{form.location.data}",
+                f"{form.open_time.data}",
+                f"{form.close_time.data}",
+                f"{form.cf_rating.data}",
+                f"{form.wf_rating.data}",
+                f"{form.pw.data}"])
+            return render_template('add.html', form=form, sent=True)
+    return render_template('add.html', form=form, sent=False)
 
 
 @app.route('/cafes')

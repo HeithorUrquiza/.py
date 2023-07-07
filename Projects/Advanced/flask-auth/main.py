8ca45b47ea44d3295f5dc7ee64f1979c8b7cf41f
Heithor Urquzia <heithoru@gmail.com>
2023-07-06 22:41:29 -0300

    blog-with-users project finished

--- Projects/Advanced/flask-auth/main.py
+++ Projects/Advanced/flask-auth/main.py
@@ -10,6 +10,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\heith\\Documents\\
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Configuring class for login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,34 +34,63 @@ def home():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        if User.query.filter_by(email=request.form.get('email')).first():
+            flash("You've already singed up with this email, log in instead!", 'error')
+            return redirect(url_for('login'))
+        
+        hash_pass = generate_password_hash(password=request.form.get('password'), method="pbkdf2:sha256", salt_length=8)
         new_user = User(
             name=request.form.get('name'),
             email=request.form.get('email'),
-            password=request.form.get('password')
+            password=hash_pass
         )
         db.session.add(new_user)
         db.session.commit()
+        
+        login_user(new_user)
+        
         return redirect(url_for('secrets'))
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(pwhash=user.password, password=password):
+                login_user(user)
+                print("Login")
+                return redirect(url_for('secrets'))
+            else:
+                flash("Password incorrect. Please try again!", 'error')
+                return redirect(url_for('login'))
+        else:
+            flash("The email doesn't exist. Please try again!", 'error')
+            return redirect(url_for('login'))
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    user = User.query.first()
-    return render_template("secrets.html", name=user.name)
+    user = current_user.name
+    print(user)
+    return render_template("secrets.html", name=user)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory('static', filename='files/cheat_sheet.pdf')
 
